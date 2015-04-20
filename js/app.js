4c8a584ac0f576bee47e6686b5fe59ade7c0bbbc
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-04-20 09:16:29 -0300

    Update 2015-04-20T12:16:29.145Z

--- js/app.js
+++ js/app.js
@@ -5,7 +5,11 @@ app = angular.module('magaperolas', ['ng', 'ngResource', 'ui.router', 'ui.bootst
 
 app.config(function($locationProvider, $stateProvider, $urlRouterProvider, ParseProvider) {
   $locationProvider.hashPrefix('!');
-  $stateProvider.state('perola', {
+  $stateProvider.state('login', {
+    url: '/login',
+    controller: 'LoginCtrl',
+    templateUrl: 'views/login.html'
+  }).state('perola', {
     url: '/',
     controller: 'PerolaCtrl',
     templateUrl: 'views/perola.html'
@@ -19,10 +23,56 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider, Parse
 });
 
 app.run(function($rootScope, $state) {
-  return $rootScope.$state = $state;
+  $rootScope.$state = $state;
+  return $rootScope.$on("$routeChangeStart", function(event, next, current) {
+    if ($rootScope.loggedInUser === null) {
+      return $location.path("/login");
+    }
+  });
 });
 
-app.controller('PerolaCtrl', function($scope, Perola) {
+app.controller('LoginCtrl', function($scope, $location, ParseAuth, CustomUser, $rootScope) {
+  $scope.loginCallback = function(user) {
+    $rootScope.loggedInUser = user;
+    return $location.path('/');
+  };
+  $scope.login = function() {
+    var callback, password, username;
+    username = $scope.user.username;
+    password = $scope.user.password;
+    callback = $scope.loginCallback;
+    return CustomUser.query({
+      'where': {
+        'username': $scope.user.username
+      }
+    }).then(function(users) {
+      if (users[0].liberado === true) {
+        return ParseAuth.login(username, password, callback);
+      } else {
+        alert("Usuário não liberado!");
+        return $location.path('/login');
+      }
+    });
+  };
+  $scope.signup = function() {
+    var password, username;
+    $scope.user.liberado = false;
+    username = $scope.user.username;
+    password = $scope.user.password;
+    if (username && password) {
+      $scope.user.save().then(function(user) {
+        return alert("Solicitação enviada com sucesso!");
+      });
+      return $scope.user = new CustomUser;
+    } else {
+      return alert("Todos os campos devem ser preenchidos!");
+    }
+  };
+  $scope.user = new CustomUser;
+  return ParseAuth.logout();
+});
+
+app.controller('PerolaCtrl', function($scope, $location, Perola, ParseAuth) {
   $scope.addPerola = function() {
     $scope.newPerola.liberada = 0;
     if ($scope.newPerola.frase && $scope.newPerola.autor) {
@@ -88,4 +138,23 @@ app.factory('Perola', function(Parse) {
     return Perola;
 
   })(Parse.Model);
+});
+
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+app.factory('CustomUser', function(ParseDefaultUser) {
+  var CustomUser;
+  return CustomUser = (function(_super) {
+    __extends(CustomUser, _super);
+
+    function CustomUser() {
+      return CustomUser.__super__.constructor.apply(this, arguments);
+    }
+
+    CustomUser.configure('users', 'username', 'password', 'liberado');
+
+    return CustomUser;
+
+  })(ParseDefaultUser);
 });
